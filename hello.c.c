2b8f5abb0d834ed2e6b9bea7bd3194462765beff
Nabil <nabilhossen58@gmail.com>
2023-04-02 12:51:56 +0600

    Hello

--- hello.c.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Mister Nabil!");
-    printf("Anyone here?");
-    printf("Who are you?");
-    return 0;
-}
---
+++ variable_practice.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int nabil = 10;
+    printf("nabil");
+    return 0;
+
+}
